8822075417bf5f51e864e6859fb31b1cf6eb4e62
Ryzz _Ganz <102559807+Ryzz123@users.noreply.github.com>
2022-09-01 13:58:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-const warnaGanti = document.getElementById("bg");
 
-warnaGanti.addEventListener("click", function() {
-   var hack = document.querySelector(".hacker");
-   hack.classList.toggle("hilang");
-})
