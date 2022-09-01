af7ef766a74abb941d5d066f452a2eda13a89957
Ryzz123 <febryananda17@gmail.com>
2022-09-01 13:38:45 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const warnaGanti = document.getElementById("bg");
+
+warnaGanti.addEventListener("click", function() {
+   var hack = document.querySelector(".hacker");
+   hack.classList.toggle("hilang");
+})
